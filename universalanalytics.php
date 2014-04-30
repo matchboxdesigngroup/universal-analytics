509bbbb5967f9cf03e786d948e88f4a21d7d71b3
Cullen Whitmore <cwhitmore@matchboxdesigngroup.com>
2014-04-30 17:04:09 -0500

    Removed custom and classic analytics from plugin

--- universalanalytics.php
+++ universalanalytics.php
@@ -6,7 +6,7 @@ Plugin Name: Universal Analytics
 
 Plugin URI: http://wordpress.org/extend/plugins/universal-analytics/
 
-Description: Adds <a href="http://www.google.com/analytics/">Google Analytics</a> tracking code on all pages. Based on plugin by Audrius Dobilinskas
+Description: Adds <a href="http://www.google.com/analytics/">Google's Universal Analytics</a> tracking code to all pages.
 
 Version: 0.1
 
@@ -54,32 +54,6 @@ function deactive_google_universal_analytics() {
 
   delete_option('tracking_off_for_role');
 
-  //classic_options
-
-  delete_option('classic_property_id');
-
-  delete_option('classic_in_footer');
-
-  delete_option('classic_plugin_switch');
-
-  delete_option('classic_tracking_off_for_role');
-
-  delete_option('classic_tracking_off_for_this_role');
-
-  
-
-  //custom_options
-
-  delete_option('custom_in_footer');
-
-  delete_option('custom_plugin_switch');
-
-  delete_option('custom_web_property_id');
-
-  delete_option('custom_tracking_off_for_role');
-
-  delete_option('custom_tracking_off_for_this_role');
-
 }
 
 
@@ -92,7 +66,7 @@ function admin_menu_google_universal_analytics() {
 
  
 
-  global  $settings_page, $settings_page1, $classic_page, $cutom_page;
+  global  $settings_page, $settings_page1;
 
   
 
@@ -102,13 +76,7 @@ function admin_menu_google_universal_analytics() {
 
  $settings_page1	=	 add_submenu_page( 'google_universal_analytics', 'Universal Analytics', 'Universal Analytics', 'manage_options', 'google_universal_analytics', 'options_page_google_universal_analytics' );
 
- $classic_page	=	 add_submenu_page( 'google_universal_analytics', 'Classic Analytics', 'Classic Analytics', 'manage_options', 'classic_analytics', 'classic_analytics_page_google_universal_analytics' );
 
- 
-
- $cutom_page	=	 add_submenu_page( 'google_universal_analytics', 'Custom Tracker', 'Custom Tracker', 'manage_options', 'custom_analytics', 'custom_analytics_page_google_universal_analytics' );
-
-  
 
 }
 
@@ -117,18 +85,6 @@ function admin_menu_google_universal_analytics() {
 function options_page_google_universal_analytics() {
 
   include(WP_PLUGIN_DIR.'/google-universal-analytics/options.php');  
-
-}
-
-function classic_analytics_page_google_universal_analytics() {
-
-  include(WP_PLUGIN_DIR.'/google-universal-analytics/classic/classic-analytics.php');  
-
-}
-
-function custom_analytics_page_google_universal_analytics() {
-
-  include(WP_PLUGIN_DIR.'/google-universal-analytics/classic/custom-analytics.php');  
 
 }
 
@@ -142,55 +98,16 @@ function google_universal_analytics() {
 
 
 
-function google_classic_analytics() {
-
-  $classic_property_id	=	get_option('classic_property_id');
-
-  $classic_analytics_code	=	"<script type='text/javascript'>
-
-
-
-  var _gaq = _gaq || [];
-
-  _gaq.push(['_setAccount', '$classic_property_id']);
-
-  _gaq.push(['_trackPageview']);
-
-
-
-  (function() {
-
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-
-  })();
-
-
-
-</script>";
-
-echo $classic_analytics_code;
-
-}
-
-function google_custom_analytics() {
-
-	echo get_option('custom_web_property_id');
-
-}
 
 
 
 function google_universal_analytics_scripts($hook){
 
-		global  $settings_page, $settings_page1, $classic_page, $cutom_page;
+		global  $settings_page, $settings_page1;
 
 		
 
-		if($hook != $settings_page && $hook != $settings_page1 && $hook != $classic_page && $hook != $cutom_page )
+		if($hook != $settings_page && $hook != $settings_page1)
 
 		return;
 
@@ -327,119 +244,6 @@ function display_google_universal_analytics_code(){
 
 
 	}
-
-	
-
-	
-
-	if(get_option('custom_tracking_off_for_role')=='on' && strcasecmp($user_role , get_option('custom_tracking_off_for_this_role'))!=0){
-
-		if (!is_admin() && get_option('custom_plugin_switch')=='on') {
-
-			if(get_option('custom_in_footer')=='on'){
-
-				add_action('wp_footer', 'google_custom_analytics');
-
-			}else{
-
-				add_action('wp_head', 'google_custom_analytics');
-
-			}
-
-		}
-
-	}elseif(is_user_logged_in() && get_option('custom_tracking_off_for_role')!='on'){
-
-		if (!is_admin() && get_option('custom_plugin_switch')=='on') {
-
-			if(get_option('custom_in_footer')=='on'){
-
-				add_action('wp_footer', 'google_custom_analytics');
-
-			}else{
-
-				add_action('wp_head', 'google_custom_analytics');
-
-			}
-
-		}
-
-
-
-	}elseif(!is_user_logged_in()){
-
-		if (!is_admin() && get_option('custom_plugin_switch')=='on') {
-
-			if(get_option('custom_in_footer')=='on'){
-
-				add_action('wp_footer', 'google_custom_analytics');
-
-			}else{
-
-				add_action('wp_head', 'google_custom_analytics');
-
-			}
-
-		}
-
-
-
-	}
-
-	
-
-	if(get_option('classic_tracking_off_for_role')=='on' && strcasecmp($user_role , get_option('classic_tracking_off_for_this_role'))!=0){
-
-		if (!is_admin() && get_option('classic_plugin_switch')=='on') {
-
-			if(get_option('classic_in_footer')=='on'){
-
-				add_action('wp_footer', 'google_classic_analytics');
-
-			}else{
-
-				add_action('wp_head', 'google_classic_analytics');
-
-			}
-
-		}
-
-	}elseif(is_user_logged_in() && get_option('classic_tracking_off_for_role')!='on'){
-
-		if (!is_admin() && get_option('classic_plugin_switch')=='on') {
-
-			if(get_option('classic_in_footer')=='on'){
-
-				add_action('wp_footer', 'google_classic_analytics');
-
-			}else{
-
-				add_action('wp_head', 'google_classic_analytics');
-
-			}
-
-		}
-
-	}elseif(!is_user_logged_in()){
-
-		if (!is_admin() && get_option('classic_plugin_switch')=='on') {
-
-			if(get_option('classic_in_footer')=='on'){
-
-				add_action('wp_footer', 'google_classic_analytics');
-
-			}else{
-
-				add_action('wp_head', 'google_classic_analytics');
-
-			}
-
-		}	
-
-	}
-
-
-
 }
 
 
@@ -500,93 +304,9 @@ function save_google_universal_analytics_settings() {
 
 
 
-function save_google_classic_analytics_settings() {
-
-	// The $_REQUEST contains all the data sent via ajax
-
-	if ( isset($_REQUEST) ) {
-
-		$classic_property_id = $_REQUEST['classic_property_id'];
-
-		$classic_in_footer = $_REQUEST['classic_in_footer'];
-
-		$classic_plugin_switch = $_REQUEST['classic_plugin_switch'];
-
-		$classic_tracking_off_for_role = $_REQUEST['classic_tracking_off_for_role'];
-
-		$classic_tracking_off_for_this_role = $_REQUEST['classic_tracking_off_for_this_role'];
-
-		
-
-		
-
-		update_option('classic_property_id', $classic_property_id);
-
-  		update_option('classic_in_footer', $classic_in_footer);
-
- 		update_option('classic_plugin_switch', $classic_plugin_switch);
-
-		update_option('classic_tracking_off_for_role', $classic_tracking_off_for_role);
-
-		update_option('classic_tracking_off_for_this_role', $classic_tracking_off_for_this_role);
-
-	}
-
-	// Always die in functions echoing ajax content
-
-   die();
-
-}
-
-function save_google_custom_analytics_settings() {
-
-	// The $_REQUEST contains all the data sent via ajax
-
-	if ( isset($_REQUEST) ) {
-
-		$custom_in_footer = $_REQUEST['custom_in_footer'];
-
-		$custom_plugin_switch = $_REQUEST['custom_plugin_switch'];
-
-		$custom_web_property_id = $_REQUEST['custom_web_property_id'];
-
-		$custom_web_property_id	=	stripslashes($custom_web_property_id);
-
-		$custom_tracking_off_for_role = $_REQUEST['custom_tracking_off_for_role'];
-
-		$custom_tracking_off_for_this_role = $_REQUEST['custom_tracking_off_for_this_role'];
-
-		
-
-		
-
-		update_option('custom_in_footer', $custom_in_footer);
-
-  		update_option('custom_plugin_switch', $custom_plugin_switch);
-
- 		update_option('custom_web_property_id', $custom_web_property_id);
-
-		update_option('custom_tracking_off_for_role', $custom_tracking_off_for_role);
-
-		update_option('custom_tracking_off_for_this_role', $custom_tracking_off_for_this_role);
-
-	}
-
-	// Always die in functions echoing ajax content
-
-   die();
-
-}
-
-
-
-
 
 add_action( 'wp_ajax_save_google_universal_analytics_settings', 'save_google_universal_analytics_settings' );
 
-add_action( 'wp_ajax_save_google_classic_analytics_settings', 'save_google_classic_analytics_settings' );
-
-add_action( 'wp_ajax_save_google_custom_analytics_settings', 'save_google_custom_analytics_settings' );
 
 
 
